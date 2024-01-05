ab4020ebb3473e34041ba692c1e7a53cb5885f3c
king-p1 <oladelex4@gmail.com>
2024-01-05 03:43:12 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import uuid from 'react-uuid'
 import './App.css';
+import Main from './Main';
+import Sidebar from './Sidebar';
+
+// import Header from './Header';
 
 function App() {
+  const [notes, setNotes] = useState(localStorage.notes ? JSON.parse(localStorage.notes) : []);
+  const [activeNote, setActiveNote] = useState(false);
+	const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    // if (localStorage.getItem('notes')){  localStorage.notes ? JSON.parse(localStorage.notes) : []
+      localStorage.setItem('notes', JSON.stringify(notes))
+  }, [notes])
+
+const onAddNote = () =>{
+const newNote = {
+  id: uuid(),
+title: "Untitled Note",
+body:"",
+lastModified:Date.now()
+}
+setNotes([newNote,...notes])
+
+}
+
+const onUpdateNote = (updatedNote) => {
+  const updatedNotesArr = notes.map((note) =>{
+    if(note.id === activeNote){
+      return updatedNote
+    }
+    return note
+  })
+  setNotes(updatedNotesArr)
+}
+
+const onDeleteNote = (idToDelete) =>{
+  // let updatedNotes = notes.filter(note=> note.id !== selectedId)
+  setNotes(notes.filter((note) =>note.id !== idToDelete))
+}
+
+const getActiveNote = () => {
+  return notes.find((note) => note.id === activeNote
+);}
+
+
   return (
+    <div className={`${darkMode && 'dark-mode'}`}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+<Sidebar notes={notes} onAddNote={onAddNote} onDeleteNote={onDeleteNote} activeNote={activeNote} setActiveNote={setActiveNote}  handleToggleDarkMode={setDarkMode}/>
+<Main activeNote={getActiveNote()}  onUpdateNote={onUpdateNote}/>
+
+</> 
+ </div>  
+</div>
   );
 }
 
---
+++ src/Sidebar.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import 'boxicons'
+
+
+
+const Sidebar = ({notes, onAddNote,onDeleteNote, activeNote, setActiveNote,handleToggleDarkMode}) => {
+  
+  const sortedNotes = notes.sort((a,b) => b.lastModified - a.lastModified)
+// const mode = document.querySelector('.bx-moon');
+  return (
+    <div className='app-sidebar'>
+
+    <div className='app-sidebar-header'>
+      <button
+				onClick={() =>
+					handleToggleDarkMode(
+						(previousDarkMode) => !previousDarkMode) 
+				} 
+				className='toggle'
+			>
+        <i class='bx bx-moon' ></i>
+        <i class='bx bx-sun' ></i>
+
+			</button>
+<h1 className='nf'>OP's Notes</h1>
+<button onClick={onAddNote}>
+<i class='bx bxs-file-plus' ></i>  </button>
+    </div>
+      <div className='app-sidebar-notes'>
+        {sortedNotes.map((note) => (
+          <div className={`app-sidebar-note  ${note.id === activeNote && 'active'}`} onClick={() => setActiveNote(note.id)}>
+        <div className='sidebar-note-title'>
+          <strong>{note.title}</strong>
+          <button onClick={() => onDeleteNote(note.id)}
+          
+          ><i class='bx bx-trash' ></i></button>
+        </div>
+        <p>{note.body && note.body.substr(0,50) + "..."}</p>
+
+
+        <small className='note-meta'>
+          Last modified {new Date(note.lastModified).toLocaleDateString("en-GB", {
+            hour:"2-digit", minute:"2-digit"
+          })}
+        </small>
+</div>
+        ))}
+      
+      </div>
+      </div>
+  )
+}
+
+
+export default Sidebar;
